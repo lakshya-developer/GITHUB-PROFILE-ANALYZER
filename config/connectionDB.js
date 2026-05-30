e4b27d02a685b1f485be86d3e7d3367d943364e3
Lakshya Verma <lakshyav221@gmail.com>
2026-05-31 02:47:47 +0530

    debug log

--- config/connectionDB.js
+++ config/connectionDB.js
@@ -12,12 +12,9 @@ const pool = mysql.createPool({
   queueLimit: 0 
 })
 
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME,
-});
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
 
 pool.getConnection((err, connection) => {
   if (err) {
